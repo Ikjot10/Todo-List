9e6260f5741864530317e8237e6c75a707711751
Ikjot Dhillon <ikjot.dhillon@mail.utoronto.ca>
2019-09-22 00:06:14 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import uuid from 'uuid';
+
+import './App.css';
+
+
+class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+  //toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id ===id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //delete todo item
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+}
+
+  //add todo
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos, newTodo]});
+}
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>This is a Todo app which was made with the help of a React Course</h1>
+        </React.Fragment>
+    )
+}
